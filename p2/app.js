d84f0a72ce3591d3a735a17c09f33925f15078e0
Rock25 <rahul.16jime049@jietjodhpur.ac.in>
2024-05-04 16:23:48 +0530

    project 3 completed

--- p2/app.js
+++ p2/app.js
@@ -5,6 +5,19 @@ btn.style.backgroundColor = 'transparent'
 
 const printCurrent = (e)=>{
     console.log('BUTTON CLICKED')
-    console.log(e)
     window.print()
 }
+
+
+const today = new Date();
+
+let day = today.getDate();
+let month = today.getMonth();
+let year  = today.getFullYear();
+
+let  monthName = ['January', 'February', 'March', 'April',' May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+let result   = `today is : ${day} of ${monthName[month]} and Year is : ${year} `
+console.log(result)
+
+const currDay = document.getElementById('currentDate');
+
